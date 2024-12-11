e678171f8cc9578d48414c4f1d7bf2fc4dde3575
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-12-11 11:08:44 +0100

    retrait popup + ajout fslightbox

--- templates-parts/card-chantier.php
+++ templates-parts/card-chantier.php
@@ -6,7 +6,7 @@ if($taxs):
     endforeach;
 endif;?>
 
-<a data-filters="<?php echo $value;?>" data-index="<?php echo get_the_id();?>" href="<?php echo get_the_post_thumbnail_url();?>" style="background:url('<?php echo get_the_post_thumbnail_url();?>');" <?php post_class('from-bottom') ;?>>
+<a fslightbox href="<?php echo get_the_post_thumbnail_url();?>" data-filters="<?php echo $value;?>" data-index="<?php echo get_the_id();?>" href="" style="background:url('<?php echo get_the_post_thumbnail_url();?>');" <?php post_class('from-bottom') ;?>>
     <article class="thumbnail">
         <h3><?php the_title();?></h3>
         <p><?php echo get_field('lieu_du_chantier');?></p>
